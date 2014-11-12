c171871cd2e001cda0ce0db8b85986161f9d66aa
Edward Arthur Bingham <eab242@cornell.edu>
2014-11-12 15:06:15 +0000

    Started work on Server

---
+++ auth.php
@@ -0,0 +1,42 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
+
+if (isset($_GET['code'])) {
+	$url = 'https://api.moves-app.com/oauth/v1/access_token';
+        $data = array('grant_type' => 'authorization_code', 'code' => $_GET['code'], 'client_id' => 'BHJJXLewp3VFBhgOY1T7NVlyXGsOtMF1', 'client_secret' => 'PnpXEjNU4xKiwp69q6pBTrva04Ez94arfCXvg9n3FxVwG5DQN7tUBnSKN7NFc5ch', 'redirect_uri' => 'http://www.sol-union.com/moves/auth.php');
+
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_URL,$url);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	$result = curl_exec ($ch);
+
+	curl_close ($ch);
+	
+	print $result;
+}
+
+if (isset($_GET['refresh_token'])) {
+	$url = 'https://api.moves-app.com/oauth/v1/access_token';
+        $data = array('grant_type' => 'refresh_token', 'refresh_token' => $_GET['refresh_token'], 'client_id' => 'BHJJXLewp3VFBhgOY1T7NVlyXGsOtMF1', 'client_secret' => 'PnpXEjNU4xKiwp69q6pBTrva04Ez94arfCXvg9n3FxVwG5DQN7tUBnSKN7NFc5ch');
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL,$url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $result = curl_exec ($ch);
+
+        curl_close ($ch);
+
+        print $result;
+}
+?>
---
+++ index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import cgi, cgitb
+
+form = cgi.FieldStorage()
+
+# headers
+print("Content-type: text/html\r\n\r\n")
+print('<html>')
+print('</html>')
