5884589a8c0e98a577dc337982d89b1c6d745a4d
Edward Arthur Bingham <eab242@cornell.edu>
2014-11-12 15:22:39 +0000

    moved authenticiation implementation to python

--- index.py
+++ index.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python
 import cgi, cgitb
+import requests
 
 form = cgi.FieldStorage()
 
 # headers
-print("Content-type: text/html\r\n\r\n")
-print('<html>')
-print('</html>')
+print("Content-type: test/html\r\n\r\n")
+
+if 'code' in form:
+	url = 'https://api.moves-app.com/oauth/v1/access_token'
+	payload = { 'grant_type' : 'authorization_code',
+		    'code' : form['code'],
+		    'client_id' : 'BHJJXLewp3VFBhgOY1T7NVlyXGsOtMF1',
+		    'client_secret' : 'PnpXEjNU4xKiwp69q6pBTrva04Ez94arfCXvg9n3FxVwG5DQN7tUBnSKN7NFc5ch', 
+		    'redirect_uri' : 'http://connect.sol-union.com/index.py' }
+	r = requests.get(url, params = payload);
+	print(r.text);
+elif 'refresh_token' in form:
+	url = 'https://api.moves-app.com/oauth/v1/access_token'
+	payload = { 'grant_type' : 'refresh_token',
+		    'refresh_token' : form['refresh_token'],
+		    'client_id' : 'BHJJXLewp3VFBhgOY1T7NVlyXGsOtMF1',
+		    'client_secret' : 'PnpXEjNU4xKiwp69q6pBTrva04Ez94arfCXvg9n3FxVwG5DQN7tUBnSKN7NFc5ch' }
+	r = requests.get(url, params = payload);
+	print(r.text);
+
