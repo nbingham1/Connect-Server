2a9048bced07f3ad1f0325df4b02470d665bda24
Edward Arthur Bingham <eab242@cornell.edu>
2014-11-30 21:53:40 +0000

    cleaned up the model and moved things into their own functions

--- model.py
+++ model.py
@@ -20,31 +20,35 @@ def get_distance(lat1, long1, lat2, long2):
 	arc = math.acos( cos )
 	return arc*6378100
 
-def normal_radii():
-	return
-
-def model(con, cur, user_id):
+def get_users(con, cur, user_id):
 	cur.execute("select u.id,u.lat,u.lon from users as u join friends as f on (u.id=f.user1_id or u.id=f.user2_id) where (f.user1_id=%s or f.user2_id=%s)", (user_id, user_id,))
 	results = cur.fetchall()
 	users = []
 	for result in results:
 		users.append({'id':result[0], 'lat':result[1], 'lon':result[2], 'last_timestamp':0, 'last_index':0, 'done':False})
+	return users
 
-	print("\n\nPlaces")
+def get_places(con, cur, user_id):
 	cur.execute("select * from places as p join friends as f inner join locations as l on p.location_id=l.id and (p.user_id=f.user1_id or p.user_id=f.user2_id) where end>%s and (f.user1_id=%s or f.user2_id=%s)", (int(time.time() - 60*60*24*7), user_id, user_id,))
-	places = cur.fetchall()
-	for place in places:
-		print(place)
+	results = cur.fetchall()
+	places = []
+	for result in results:
+		places.append({'user_id':result[0], 'start_time':result[2], 'end_time':result[3], 'name':result[4], 'lat':result[10], 'lon':result[11]})
+	return places
 
-	print("\n\nFriends")
+def get_friends(con, cur, user_id):
 	cur.execute("select * from friends where user1_id = %s or user2_id = %s", (int(user_id), int(user_id),))
 	results = cur.fetchall()
 	friends = []
 	for friend in results:
-		friends.append({'user1_id':friend[0], 'user2_id': friend[1], 'normal_radius':0, 'data_count':0})	
-	print(friends)
+		friends.append({'user1_id':friend[0], 'user2_id': friend[1]})	
+	return friends
 
-	print("\n\nUsers")
+def normal_radii(users, places, friends):
+	for friend in friends:
+		friend['normal_radius'] = 0
+		friend['data_count'] = 0
+
 	done = False
 	while (not done):
 		min_timestamp = time.time()
@@ -59,8 +63,8 @@ def model(con, cur, user_id):
 		min_place = -1
 
 		for i in range(0, len(places)):
-			if places[i][0] == users[min_user]['id'] and places[i][3] > users[min_user]['last_timestamp'] and places[i][3] < min_timestamp:
-				min_timestamp = places[i][3]
+			if places[i]['user_id'] == users[min_user]['id'] and places[i]['end_time'] > users[min_user]['last_timestamp'] and places[i]['end_time'] < min_timestamp:
+				min_timestamp = places[i]['end_time']
 				min_place = i				
 
 		if min_place == -1:
@@ -77,13 +81,24 @@ def model(con, cur, user_id):
 				if users[min_user]['id'] == friend['user1_id'] or users[min_user]['id'] == friend['user2_id']:
 					for user in users:
 						if user['id'] != users[min_user]['id'] and (user['id'] == friend['user1_id'] or user['id'] == friend['user2_id']):
-							friend['normal_radius'] += get_distance(places[users[min_user]['last_index']][10], places[users[min_user]['last_index']][11], places[user['last_index']][10], places[user['last_index']][11])
+							friend['normal_radius'] += get_distance(places[users[min_user]['last_index']]['lat'], places[users[min_user]['last_index']]['lon'], places[user['last_index']]['lat'], places[user['last_index']]['lon'])
 							friend['data_count'] += 1
 
-	print("\n\nResults")
 	for friend in friends:
 		friend['normal_radius'] /= friend['data_count']
 		friend.pop('data_count', None)
+	return
+
+def current_radii(users, friends):
+	return	
+
+def model(con, cur, user_id):
+	users = get_users(con, cur, user_id)
+	places = get_places(con, cur, user_id)
+	friends = get_friends(con, cur, user_id)
+	normal_radii(users, places, friends)
+	
+	for friend in friends:
 		print(friend)
 
 	return
