a474219e7068f71168501d0b8cbbe5665f6b1ba4
Fanxing Meng <fm369@cornell.edu>
2014-11-30 20:38:58 -0500

    send interests

---
+++ interests.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import cgi, cgitb
+import requests
+import MySQLdb as db
+import time
+import datetime
+from datetime import datetime
+import json
+import sys
+import re
+
+
+sys.stderr = sys.stdout
+
+con = db.connect('localhost', 'connect', 'socialize', 'connect')
+cur = con.cursor()
+
+form = cgi.FieldStorage()
+
+print("Content-type: text/plain\r\n")
+
+if 'user' in form and 'json' in form:
+	print(form['json'].value)
+	
+	
+	json_data = json.loads(form['json'].value)
+		
