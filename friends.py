b9b7d5ee9ad32b96c67a62c223fa7fafe3604098
Edward Arthur Bingham <eab242@cornell.edu>
2014-12-01 04:15:26 +0000

    got friend list upload working

--- friends.py
+++ friends.py
@@ -24,11 +24,26 @@ if 'facebook' in form and 'user' in form and 'name' in form:
 	con.commit()
 	cur.execute("select * from users")
 	results = cur.fetchall()
-	print(results)
 
 if 'user' in form and 'json' in form:
-	print(form['json'].value)
-	
-	
 	json_data = json.loads(form['json'].value)
-		
+	friends = json_data['data']
+	ids = []
+	query = "select facebook,id from users where facebook in ("
+	for i in range(0, len(friends)-1):
+		query += "%s,"
+	query += "%s)"
+
+	for friend in friends:
+		ids.append(friend['id'])
+
+	cur.execute(query, tuple(ids))
+	results = cur.fetchall()
+	
+	for result in results:
+		if long(form['user'].value) < long(result[1]):
+			cur.execute("insert into friends (user1_id,user2_id,health,last_update) values (%s, %s, 0, 0) on duplicate key update user1_id=user1_id", (long(form['user'].value), long(result[1]),))
+			con.commit()
+		elif long(form['user'].value) != long(result[1]):
+			cur.execute("insert into friends (user1_id,user2_id,health,last_update) values (%s, %s, 0, 0) on duplicate key update user1_id=user1_id", (long(result[1]), long(form['user'].value),))
+			con.commit()
